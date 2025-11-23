3cf5538f389a336d0bbba05b7833ebc8fb1a3793
d1scipline <143661704+d1scipline@users.noreply.github.com>
2025-11-23 22:27:34 +0300

    create item and project classes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 import "./styles.css";
 
-console.log("Hello world!");
+class todoItem {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+class Project {
+  constructor(name) {
+    this.name = name;
+    this.todoList = [];
+  }
+
+  addItem(item) {
+    this.todoList.push(item);
+  }
+
+  removeItem(item) {
+    const index = this.todoList.indexOf(item);
+    if (index > -1) {
+      this.todoList.splice(index, 1);
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
+
+const newProject = new Project("Build Robot");
+const item1 = new todoItem(
+  "Buy arduino",
+  "Order an arduino online",
+  "25/11/2025",
+  3
+);
+const item2 = new todoItem(
+  "Get 3D Printer",
+  "Buy a 3d printer",
+  "26/11/2025",
+  2
+);
+newProject.addItem(item1);
+newProject.addItem(item2);
+console.log(JSON.parse(JSON.stringify(newProject.todoList)));
+newProject.removeItem(item2);
+console.log(newProject.todoList);
